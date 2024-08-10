e0b193c38c39b41e94e39bd93002bc26edb58476
zkzong <zongzhankui@hotmail.com>
2024-08-10 14:14:48 +0800

    groovy

---
+++ src/main/groovy/org/example/Example.groovy
@@ -0,0 +1,80 @@
+package org.example
+
+/**
+ * @Author: zongz
+ * @Date: 2024/8/10
+ */
+class Example {
+
+//    static void main(String[] args) {
+//        // One can see the use of a semi-colon after each statement
+//        def x = 5;
+//        // Using a simple println statement to print output to the console
+//        println('Hello World');
+//    }
+
+//    static void main(String[] args) {
+//        //Example of a int datatype
+//        int x = 5;
+//
+//        //Example of a long datatype
+//        long y = 100L;
+//
+//        //Example of a floating point datatype
+//        float a = 10.56f;
+//
+//        //Example of a double datatype
+//        double b = 10.5e40;
+//
+//        //Example of a BigInteger datatype
+//        BigInteger bi = 30g;
+//
+//        //Example of a BigDecimal datatype
+//        BigDecimal bd = 3.5g;
+//
+//        println(x);
+//        println(y);
+//        println(a);
+//        println(b);
+//        println(bi);
+//        println(bd);
+//    }
+
+//    static void main(String[] args) {
+//        // x is defined as a variable
+//        String x = "Hello";
+//
+//        // The value of the variable is printed to the console
+//        println(x);
+//    }
+
+//    static void main(String[] args) {
+//        // Defining a variable in lowercase
+//        int x = 5;
+//
+//        // Defining a variable in uppercase
+//        int X = 6;
+//
+//        // Defining a variable with the underscore in it's name
+//        def _Name = "Joe";
+//
+//        println(x);
+//        println(X);
+//        println(_Name);
+//    }
+
+//    static void main(String[] args) {
+//        //Initializing 2 variables
+//        int x = 5;
+//        int X = 6;
+//
+//        //Printing the value of the variables to the console
+//        println("The value of x is " + x + "The value of X is " + X);
+//    }
+
+    static void main(String[] args) {
+        def range = 5..10;
+        println(range);
+        println(range.get(2));
+    }
+}
---
+++ src/main/groovy/org/example/Main.groovy
@@ -0,0 +1,9 @@
+package org.example
+
+/**
+ * @Author: zongz
+ * @Date: 2024/8/10
+ */
+static void main(String[] args) {
+    println "Hello world!"
+}
